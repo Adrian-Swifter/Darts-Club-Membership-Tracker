525d8cbc8a80769bac805d24cabce24091d13656
adrian-swifter <milosffh@yahoo.com>
2021-08-30 18:04:46 +0200

    Wrapped app in navContainer and added nav depend

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import Homepage from "./app/screens/Homepage";
 
 export default function App() {
   return (
-    <Homepage/>
+    <NavigationContainer>
+      <Homepage />
+    </NavigationContainer>
   );
 }
